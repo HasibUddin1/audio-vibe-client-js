2fddd95aeb797f9e1e0dd058c2120d1a1f03444b
IconicLuck <uhasib959@gmail.com>
2023-08-22 13:27:42 +0600

    hid the firebase config information

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCe5z3JJPR4xIVoVzSQa9MIfIR4jzkJcIg",
-  authDomain: "audio-vibe-dd46f.firebaseapp.com",
-  projectId: "audio-vibe-dd46f",
-  storageBucket: "audio-vibe-dd46f.appspot.com",
-  messagingSenderId: "461619501391",
-  appId: "1:461619501391:web:2ab7c2eca3ce7925880d3a"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
