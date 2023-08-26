2f1feb92d14e58e8cd131d8a9af1fcb5dd5c8aaf
IconicLuck <uhasib959@gmail.com>
2023-08-26 17:56:09 +0600

    changed the title and banner image for this website

--- src/hooks/useTitle.jsx
+++ src/hooks/useTitle.jsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 
 const useTitle = title => {
     useEffect(() => {
-        document.title = `Audio Hub | ${title}`
+        document.title = `Audio Vibe | ${title}`
     }, [title])
 }
 
